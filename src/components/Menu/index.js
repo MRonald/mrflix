036eb2aacb8b6e1f0c1ee03ff62195a72c44d8de
Michael Ronald <ronaldmichael918@gmail.com>
2020-08-01 13:19:19 -0300

    Criando o componente ButtonLink e instalando Styled-components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
+import ButtonLink from '../ButtonLink';
 
 function Menu() {
     return (
@@ -8,6 +9,9 @@ function Menu() {
             <a href="/">
                 <img src={Logo} alt="MRFlix Logo" className="Logo"/>
             </a>
+            <ButtonLink className="ButtonLink" href="/">
+                Novo Vídeo
+            </ButtonLink>
         </nav>
     );
 }
