a2ed48cb4c71af8049886a628ab8c84803b248e1
Michael Ronald <ronaldmichael918@gmail.com>
2020-08-02 20:57:36 -0300

    Inserindo o react-router-dom, reorganizando pages e inserindo novas pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import Pagina404 from './pages/error404';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={Home} exact/>
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/error404/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Pagina404 = () => (<div>404 Error</div>);
+
+export default Pagina404;
