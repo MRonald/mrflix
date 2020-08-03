3b7c0f3ae31b0ca063343b27a771eb7da44dd863
Michael Ronald <ronaldmichael918@gmail.com>
2020-08-03 10:16:35 -0300

    Mudando o estilo do PageDefault e do index.css para deixar o app em tela cheia | Finalizando parte de navegacao

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PageDefault from '../../../components/PageDefault';
+import { Link } from 'react-router-dom';
 
 function Categoria() {
     return (
-        <div>
-            <PageDefault>
-                Cadastro de Categoria
-            </PageDefault>
-        </div>
+        <PageDefault>
+            <h1>Cadastro de Categoria</h1>
+            <Link to="/">
+                Ir para a Home
+            </Link>
+        </PageDefault>
     );
 }
 
