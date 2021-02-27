3382cdbdb13f7f416a9c8b73065ef25a6a4a7040
MRonald <ronaldmichael918@gmail.com>
2021-02-27 19:23:05 -0300

    Atualizações na página de cadastro de categoria | Configuração do server.js para deploy na Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonSever = require('json-server');
+const server = jsonSever.create();
+const router = jsonSever.router('db.json');
+const middlewares = jsonSever.defaults();
+
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+    console.log(`JSON Server is running in ${port}`);
+});
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -21,6 +21,16 @@ function CadastroCategoria() {
       [chave]: valor, // nome: 'valor'
     })
   }
+
+  useEffect(() => {
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL).then(async (respostaDoServidor) => {
+      const resposta = await respostaDoServidor.json();
+      setCategorias([
+        ...resposta,
+      ]);
+    })
+  }, []);
 
   function handleChange(infosDoEvento) {
     setValue(
@@ -94,6 +104,11 @@ function CadastroCategoria() {
         </Button>
       </form>
       
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
         {categorias.map((categoria, indice) => {
